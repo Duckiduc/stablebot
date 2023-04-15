539944d8ab70544fc49f87ce196a1ef47e5b7d96
Duckiduc <git@ducki.dev>
2023-04-15 15:31:23 +0200

    Init Discord bot with stable diffusion diffusers

---
+++ stablebot.py
@@ -0,0 +1,92 @@
+import discord
+from discord.ext import commands
+from diffusers import StableDiffusionPipeline
+from dotenv import dotenv_values
+import torch
+from threading import Thread
+import io
+import time
+
+
+class StableDiffusionGenerator(Thread):
+    def __init__(
+        self,
+        prompt: str,
+        model: str,
+        cuda: str,
+        height: int = 512,
+        width: int = 512,
+        iter: int = 50,
+    ):
+        super().__init__()
+        self.prompt = prompt
+        self.model = model
+        self.cuda = cuda
+        self.height = height
+        self.width = width
+        self.iter = iter
+
+    def run(self):
+        print("Loading model")
+        pipe = StableDiffusionPipeline.from_pretrained(
+            self.model, torch_dtype=torch.float16
+        )
+        pipe.enable_xformers_memory_efficient_attention()
+        pipe = pipe.to(self.cuda)
+        print("Generating image")
+        self.image = pipe(
+            self.prompt,
+            height=self.height,
+            width=self.width,
+            num_inference_steps=self.iter,
+        ).images[0]
+
+config = dotenv_values(".env")
+
+description = """A little Stable Diffusion bot"""
+
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="/", description=description, intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
+    print("------")
+
+@bot.command()
+async def foo(ctx, arg):
+    await ctx.send(arg)
+
+@bot.command()
+async def generate(
+    ctx, prompt: str, height: int = 512, width: int = 512, iter: int = 50
+):
+    """Stable diffusion generate"""
+    await ctx.send("Ok")
+    thread = StableDiffusionGenerator(
+        prompt=prompt,
+        # model="prompthero/openjourney",  # "andite/anything-v4.0",
+        # model="dreamlike-art/dreamlike-photoreal-2.0",
+        # model="prompthero/openjourney-v4",
+        model="MirageML/lowpoly-world",
+        cuda="cuda:0",
+        # cuda="cpu",
+        height=height,
+        width=width,
+        iter=iter,
+    )
+    thread.start()
+    while thread.is_alive():
+        time.sleep(2)
+    image = thread.image
+    with io.BytesIO() as image_binary:
+        image.save(image_binary, "PNG")
+        image_binary.seek(0)
+        await ctx.send(file=discord.File(fp=image_binary, filename="image.png"))
+    thread.join()
+
+bot.run(config["DISCORD_TOKEN"])
